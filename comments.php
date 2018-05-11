2e22f889b0a3ada8b1c460f5b9d6a902c653c234
msalam <ms17alam@gmail.com>
2018-05-11 11:57:08 -0400

    login file

---
+++ comments.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+
+<?php
+	
+	echo pow(2, 2);
+	echo rand();
+	
+	$string = "Hello World";
+	echo "<br>";
+	echo strtolower($string);
+	
+	
+	?>
+
+	
+</body>
+</html>
---
+++ dynamic_text.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+
+<?php 
+	
+	$pageTitle = "This is my Portfolio..!!";
+	?>
+	
+	
+	<h1><?php echo $pageTitle; ?></h1>
+	
+</body>
+</html>

73a4109bae4870e0e7eb7821dcca4e1870a7e3ba
msalam <ms17alam@gmail.com>
2018-05-13 13:36:53 -0400

    udate users info through ID

---
+++ mysql/functions.php
@@ -0,0 +1,20 @@
+<?php 
+
+function showAllData(){
+$query = "SELECT * FROM users";
+	$result = mysqli_query($connection, $query);
+	
+	if(!$result){
+		die('Query Failed' . mysqli_error());
+	}
+	
+	while($row = mysqli_fetch_assoc($result)){
+					    
+	$id = $row['id'];
+	echo "<option             value='$id'>$id</option>";
+		
+	}
+						
+}
+
+?>
---
+++ mysql/login_update.php
@@ -0,0 +1,57 @@
+<?php include "db.php"?>
+<?php include "functions.php";?>
+
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>User Login</title>
+	
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
+</head>
+<body>
+	
+	<div class="container">
+		<div class="col-sm-6">
+		
+			
+			<form action="login_create.php" method="post">
+				<div class="form-group">
+				<lebel for="username">User Name</lebel>
+				<input type="text" name="username" class="form-control">
+				</div>
+				<div class="form-group">
+				<lebel for="password">Password</lebel>
+				<input type="password" name ="password" class="form-control">
+				</div>
+				
+				<div class="form-group">
+				
+					<select name="id" id="">
+					
+					<?php
+						
+					showAllData();
+						
+						?>
+
+					</select>	
+					
+				</div>
+				
+				<input class="btn-primary" type="submit" name="submit" value="submit" value="UPDATE">
+			</form>
+			
+			
+			
+		</div>
+		
+	</div>
+
+	
+</body>
+</html>
